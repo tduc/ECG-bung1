dbc9a7f8604c8b99d5379fdc8d87772313d8c0b3
Hung <hung.tduc@yahoo.com>
2014-04-28 17:13:54 +0200

    testübung
    push-test

---
+++ ECGÜbung1.cpp
@@ -0,0 +1,14 @@
+// ECGÜbung1.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
